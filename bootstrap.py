22bb44d5581826f3fc54c8500ed6c3c282b4e67e
yongjei86 <winder86@gmail.com>
2026-09-14 20:16:28 +0900

    Run Hyeon Waenyamyeon reading import

--- bootstrap.py
+++ bootstrap.py
@@ -35,4 +35,5 @@ c.commit(); c.close()
 import seed_riley_reading
 import add_riley_jeoncheondang18
 import add_hyeon_reading_20260914
+import add_hyeon_waenyamyeon_20260914
 print('SQLite schema ready:',DB)
