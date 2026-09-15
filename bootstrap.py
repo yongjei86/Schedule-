dbddf20ededf05286df1d7496eef3133aa53d708
yongjei86 <winder86@gmail.com>
2026-09-15 21:03:47 +0900

    Run Hyeon Sep 15 SayPen reading import

--- bootstrap.py
+++ bootstrap.py
@@ -37,4 +37,5 @@ import add_riley_jeoncheondang18
 import add_hyeon_reading_20260914
 import add_hyeon_waenyamyeon_20260914
 import add_hyeon_winter_is_here_20260914
+import add_hyeon_reading_20260915
 print('SQLite schema ready:',DB)
